ee53b5cda96ae896295e836e1ff49d20565d15a1
Liz Escobar <elizabethescobar@Lizs-MacBook-Pro.local>
2024-01-07 07:26:24 -0500

    Fixing ts problem.

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,4 +1,4 @@
 import { Inter } from 'next/font/google';
 import { Lusitana } from 'next/font/google';
-export const lusitana = Inter({ subsets: ['latin'], weigth: [400, 700] });
+export const lusitana = Inter({ subsets: ['latin'], weight: [400, 700] });
 export const inter = Inter({ subsets: ['latin'] });
